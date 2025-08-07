fdd708b2431e07409327c63603b483fecb47289e
akatsuki_dya <dojun2029@hanmail.net>
2025-08-08 04:48:41 +0900

    modify_commit

--- src/pages/Hompage/components/banner/Banner.jsx
+++ src/pages/Hompage/components/banner/Banner.jsx
@@ -1,12 +1,11 @@
+// Banner.jsx
 import React from 'react'
 import { useMoviesQuery } from '../../../../hooks/useMoviesQuery'
 import Alert from 'react-bootstrap/Alert';
 import './Banner.style.css'
 
 const Banner = () => {
-
   const {data,isLoading,isError,error} = useMoviesQuery('popular')
-  console.log("data",data)
 
   if(isLoading) {
     return <div>Loading...</div>
@@ -14,17 +13,15 @@ const Banner = () => {
     return <Alert variant='danger'>Error : {error.message}</Alert>
   }
 
-
   return (
     <div style={{
       backgroundImage : "url(" + `https://media.themoviedb.org/t/p/w1066_and_h600_bestv2/${data.results[0].poster_path}` + ")"
     }}
     className='banner'
     >
-
       <div className='text-white banner-text-area'>
-        <h1>{data?.results[0].title}</h1>
-        <h3>{data?.results[0].overview}</h3>
+        <h1 className='banner-title'>{data?.results[0].title}</h1>
+        <p className='banner-overview'>{data?.results[0].overview}</p>
       </div>
     </div>
   )

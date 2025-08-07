b3d7d01df225bc1edb1909414adeb7148b78bfcc
akatsuki_dya <dojun2029@hanmail.net>
2025-08-08 04:32:40 +0900

    last_commit

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_KEY = import.meta.env.VITE_API_KEY
 
 const api = axios.create({
-    baseURL: "/api",//"https://api.themoviedb.org/3",
+    baseURL: 'https://api.themoviedb.org/3',//"/api",//"https://api.themoviedb.org/3",
     headers: {
         Accept: "application/json",
         Authorization: `Bearer ${API_KEY}`,
